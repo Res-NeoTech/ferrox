8bcbf0efff34ef4207ea660f43b43219756f3589
NeoTech <ptysiamaksim@gmail.com>
2026-03-05 15:56:22 +0100

    feat: basic file serving, only html for now.

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,36 @@
-use std::net::{TcpListener, TcpStream};
+use std::fs::File;
 use std::io::{Read, Write};
+use std::net::{TcpListener, TcpStream};
+use std::path::Path;
 
 fn handle_connection(mut stream: TcpStream) {
     let mut buffer: [u8; 1024] = [0; 1024];
 
     stream.read(&mut buffer).unwrap();
 
-    println!("{}", String::from_utf8_lossy(&buffer));
+    let request: std::borrow::Cow<'_, str> = String::from_utf8_lossy(&buffer);
+    let first_line = request.lines().next().unwrap();
+    let parts: Vec<&str> = first_line.split_whitespace().collect();
 
-    let response: &str = "HTTP/1.1 200 OK\r\n\r\nHello, Ferrox!";
+    let path = Path::new("www/index.html");
+    let display = path.display();
+
+    let mut file = match File::open(&path) {
+        Err(why) => panic!("couldn't open {}: {}", display, why),
+        Ok(file) => file,
+    };
+
+    let mut s = String::new();
+    match file.read_to_string(&mut s) {
+        Err(why) => panic!("couldn't read {}: {}", display, why),
+        Ok(_) => print!("{} contains:\n{}", display, s),
+    }
+
+    let (method, path, version) = (parts[0], parts[1], parts[2]);
+
+    println!("Method: {}\nPath: {}\nVersion: {}", method, path, version);
+
+    let response = format!("HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n{}", s);
 
     stream.write(response.as_bytes()).unwrap();
     stream.flush().unwrap();
@@ -17,7 +39,7 @@ fn handle_connection(mut stream: TcpStream) {
 fn main() {
     let listener: TcpListener = TcpListener::bind("127.0.0.1:80").unwrap();
 
-    println!("Server running on http://127.0.0.1:80");
+    println!("Ferrox running on http://127.0.0.1:80");
 
     for stream in listener.incoming() {
         let stream: TcpStream = stream.unwrap();
